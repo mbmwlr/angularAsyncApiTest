c075cf6bd8e29ac86ba41a901112cac2d3e77f24
Michael Obermueller (FG-643) <Michael.Obermueller@bmw.de>
2023-05-02 15:19:17 +0200

    mvp angular react csp error asyncapi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,307 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+//@ts-ignore
+import AsyncApiStandalone from '@asyncapi/react-component/browser/standalone';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularAsyncApiTest';
+  unsafeEval = false;  //------- modify this to toggle unsafe-eval for script src in CSP -------
+  schema = `
+  asyncapi: '2.0.0'
+  id: 'urn:com:smartylighting:streetlights:server'
+  info:
+    title: Streetlights API
+    version: '1.0.0'
+    description: |
+      The Smartylighting Streetlights API allows you to remotely manage the city lights.
+
+      ### Check out its awesome features:
+
+      * Turn a specific streetlight on/off 
+      * Dim a specific streetlight 
+      * Receive real-time information about environmental lighting conditions 
+    license:
+      name: Apache 2.0
+      url: https://www.apache.org/licenses/LICENSE-2.0
+
+  servers:
+    production:
+      url: api.streetlights.smartylighting.com:{port}
+      protocol: mqtt
+      description: Test broker
+      variables:
+        port:
+          description: Secure connection (TLS) is available through port 8883.
+          default: '1883'
+          enum:
+            - '1883'
+            - '8883'
+      security:
+        - apiKey: []
+        - supportedOauthFlows:
+          - streetlights:on
+          - streetlights:off
+          - streetlights:dim
+        - openIdConnectWellKnown: []
+      bindings:
+        mqtt:
+          clientId: guest        
+          cleanSession: false
+          keepAlive: 60
+          bindingVersion: 0.1.0
+          lastWill:
+            topic: smartylighting/streetlights/1/0/lastwill
+            qos: 1
+            message: so long and thanks for all the fish
+            retain: false
+
+  defaultContentType: application/json
+
+  channels:
+    smartylighting/streetlights/1/0/event/{streetlightId}/lighting/measured:
+      description: The topic on which measured values may be produced and consumed.
+      parameters:
+        streetlightId:
+          $ref: '#/components/parameters/streetlightId'
+      bindings:
+        ws:
+          method: 'POST'
+          bindingVersion: '0.1.0'
+      subscribe:
+        summary: Receive lighting conditions of a streetlight.
+        description: Receive information about environmental lighting conditions of a particular streetlight.
+        operationId: receiveLightMeasurement
+        traits:
+          - $ref: '#/components/operationTraits/kafka'
+        message:
+          $ref: '#/components/messages/lightMeasured'
+        bindings:
+          mqtt:
+            qos: 1
+            bindingVersion: 0.1.0
+          http:
+            type: request
+            method: GET
+            query:
+              type: object
+              required:
+              - companyId
+              properties:
+                companyId:
+                  type: number
+                  minimum: 1
+                  description: The Id of the company.
+              additionalProperties: false
+      publish:
+        summary: Send information about environmental lighting conditions of a particular streetlight.
+        operationId: sendLightMeasurement
+        traits:
+          - $ref: '#/components/operationTraits/kafka'
+        message:
+          $ref: '#/components/messages/lightMeasured'
+
+    smartylighting/streetlights/1/0/action/{streetlightId}/turn/on:
+      parameters:
+        streetlightId:
+          $ref: '#/components/parameters/streetlightId'
+      publish:
+        operationId: turnOn
+        traits:
+          - $ref: '#/components/operationTraits/kafka'
+        message:
+          $ref: '#/components/messages/turnOnOff'
+        bindings:
+          mqtt:
+            qos: 1
+            bindingVersion: 0.1.0
+
+    smartylighting/streetlights/1/0/action/{streetlightId}/turn/off:
+      parameters:
+        streetlightId:
+          $ref: '#/components/parameters/streetlightId'
+      publish:
+        operationId: turnOff
+        traits:
+          - $ref: '#/components/operationTraits/kafka'
+        message:
+          $ref: '#/components/messages/turnOnOff'
+
+    smartylighting/streetlights/1/0/action/{streetlightId}/dim:
+      parameters:
+        streetlightId:
+          $ref: '#/components/parameters/streetlightId'
+      publish:
+        operationId: dimLight
+        traits:
+          - $ref: '#/components/operationTraits/kafka'
+        message:
+          $ref: '#/components/messages/dimLight'
+
+  components:
+    messages:
+      lightMeasured:
+        name: lightMeasured
+        title: Light measured
+        summary: Lighting conditions for a streetlight.
+        description: Inform about environmental lighting conditions for a particular streetlight.
+        contentType: application/json
+        bindings:
+          mqtt:
+            bindingVersion: 0.1.0
+        traits:
+          - $ref: '#/components/messageTraits/commonHeaders'
+        payload:
+          $ref: "#/components/schemas/lightMeasuredPayload"
+        examples:
+          - headers:
+              my-app-header: 12
+            payload:
+              lumens: 1
+              sentAt: 2020-01-31T13:24:53Z
+          - headers:
+              my-app-header: 13
+          - payload:
+              lumens: 3
+              sentAt: 2020-10-31T13:24:53Z
+      turnOnOff:
+        name: turnOnOff
+        title: Turn on/off
+        summary: Command a particular streetlight to turn the lights on or off.
+        traits:
+          - $ref: '#/components/messageTraits/commonHeaders'
+        payload:
+          $ref: "#/components/schemas/turnOnOffPayload"
+      dimLight:
+        name: dimLight
+        title: Dim light
+        summary: Command a particular streetlight to dim the lights.
+        traits:
+          - $ref: '#/components/messageTraits/commonHeaders'
+        payload:
+          $ref: "#/components/schemas/dimLightPayload"
+
+    schemas:
+      lightMeasuredPayload:
+        type: object
+        properties:
+          lumens:
+            type: integer
+            minimum: 0
+            description: Light intensity measured in lumens.
+          sentAt:
+            $ref: "#/components/schemas/sentAt"
+        required:
+          - lumens
+      turnOnOffPayload:
+        type: object
+        properties:
+          command:
+            type: string
+            enum:
+              - on
+              - off
+            description: Whether to turn on or off the light.
+          sentAt:
+            $ref: "#/components/schemas/sentAt"
+      dimLightPayload:
+        type: object
+        properties:
+          percentage:
+            type: integer
+            description: Percentage to which the light should be dimmed to.
+            minimum: 0
+            maximum: 100
+          sentAt:
+            $ref: "#/components/schemas/sentAt"
+      sentAt:
+        type: string
+        format: date-time
+        description: Date and time when the message was sent.
+
+    securitySchemes:
+      apiKey:
+        type: apiKey
+        in: user
+        description: Provide your API key as the user and leave the password empty.
+      supportedOauthFlows:
+        type: oauth2
+        description: Flows to support OAuth 2.0
+        flows:
+          implicit:
+            authorizationUrl: 'https://authserver.example/auth'
+            scopes:
+              'streetlights:on': Ability to switch lights on
+              'streetlights:off': Ability to switch lights off
+              'streetlights:dim': Ability to dim the lights
+          password:
+            tokenUrl: 'https://authserver.example/token'
+            scopes:
+              'streetlights:on': Ability to switch lights on
+              'streetlights:off': Ability to switch lights off
+              'streetlights:dim': Ability to dim the lights
+          clientCredentials:
+            tokenUrl: 'https://authserver.example/token'
+            scopes:
+              'streetlights:on': Ability to switch lights on
+              'streetlights:off': Ability to switch lights off
+              'streetlights:dim': Ability to dim the lights
+          authorizationCode:
+            authorizationUrl: 'https://authserver.example/auth'
+            tokenUrl: 'https://authserver.example/token'
+            refreshUrl: 'https://authserver.example/refresh'
+            scopes:
+              'streetlights:on': Ability to switch lights on
+              'streetlights:off': Ability to switch lights off
+              'streetlights:dim': Ability to dim the lights
+      openIdConnectWellKnown:
+        type: openIdConnect
+        openIdConnectUrl: 'https://authserver.example/.well-known'
+
+    parameters:
+      streetlightId:
+        description: The ID of the streetlight.
+        schema:
+          type: string
+
+    messageTraits:
+      commonHeaders:
+        headers:
+          type: object
+          properties:
+            my-app-header:
+              type: integer
+              minimum: 0
+              maximum: 100
+          required:
+            - my-app-header
+    
+    operationTraits:
+      kafka:
+        bindings:
+          kafka:
+            clientId:
+              type: string
+              enum: ['my-app-id']
+            bindingVersion: '0.1.0'
+  `;
+
+  constructor(private element: ElementRef, private meta: Meta) {
+    this.meta.addTag({
+      'http-equiv': 'Content-Security-Policy',
+      content:
+        `default-src 'none'; script-src 'self'${this.unsafeEval ? " 'unsafe-eval'" : ""}; connect-src 'self'; img-src 'self'; style-src 'self' 'unsafe-inline'; font-src 'self'; media-src 'self'; base-uri 'self'; form-action 'self';`,
+    });
+  }
+  ngAfterViewInit() {
+    const config = {}; // Configuration for component. This same as for normal React component
+    const container = this.element.nativeElement.querySelector('#asyncapi-doc');
+    AsyncApiStandalone.render({ schema: this.schema, config }, container);
+  }
+
+  ngOnDestroy(): void {
+    this.element.nativeElement.querySelector('#asyncapi-doc').remove();
+  }
 }
